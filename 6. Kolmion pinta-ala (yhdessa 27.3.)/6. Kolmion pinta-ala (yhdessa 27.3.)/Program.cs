ac05c9720d9e214cb1f06de6a254879ad65ec0c7
Annele1 <anne.leskinen@edu.sakky.fi>
2023-04-10 20:21:01 +0300

    6 C# Perus: Kolmion pinta-ala(yhdessä 27.3.)

---
+++ 6. Kolmion pinta-ala (yhdessa 27.3.)/6. Kolmion pinta-ala (yhdessa 27.3.)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Kolmion_pinta_ala__yhdessa_27._3._
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // 6 C# Perus: Kolmion pinta-ala(yhdessä 27.3.)
+        // https://moodle.sakky.fi/mod/assign/view.php?id=1159590
+
+        //// Ilmoitetaan käyttäjälle, mitä tietoa pyydetään
+        //Console.Write("Anna kolmion kanta: ");
+        //// Luetaan käyttäjältä arvo string muodossa
+        //// Ja muokataan string -> double muotoon
+        //double triangleBase = double.Parse(Console.ReadLine());
+
+        //Console.Write("Anna kolmion korkeus: ");
+        //double triangleHeigth = double.Parse(Console.ReadLine());
+
+        //// Voidaan näyttää laskutoimituksen lopputulos suoraan osana tekstiä. Huomioi "Magic Number" -2-
+        //Console.WriteLine($"Kolmion pinta-ala on: {triangleBase* triangleHeigth / 2}");
+
+        //// Pysäytetään sovellus
+        //Console.WriteLine();
+        //Console.WriteLine("Press any key to exit");
+        //Console.ReadKey();
+        {
+            Console.Write("Anna kolmion kanta: ");
+            double triangleBase = double.Parse(Console.ReadLine());
+            Console.Write("Anna kolmion korkeus: ");
+            double triangleHeigth = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Kolmion pinta-ala on: {triangleBase * triangleHeigth / 2}");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
